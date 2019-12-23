a5e888107c7d9d38c2437a35fcfc7902c1babd8b
Igor Gabriel <igor.gabriel.saturnino@gmail.com>
2019-12-23 14:51:36 -0300

    criacao do projeto

---
+++ ThunderCoin/src/test/java/com/invillia/ThunderCoin/ThunderCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.ThunderCoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
