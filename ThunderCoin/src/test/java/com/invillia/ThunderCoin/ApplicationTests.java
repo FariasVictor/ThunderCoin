7fa574c2b6f64a00f5e23b44a97346445384d100
Igor Gabriel <igor.gabriel.saturnino@gmail.com>
2019-12-23 15:07:47 -0300

    configuracao inicial do projeto ThunderCoin

--- ThunderCoin/src/test/java/com/invillia/ThunderCoin/ApplicationTests.java
+++ ThunderCoin/src/test/java/com/invillia/ThunderCoin/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ThunderCoinApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
